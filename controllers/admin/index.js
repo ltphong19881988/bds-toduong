cb58e10f883b9ede4b6ae2409378d905a55b879b
unknown <ltphong1988@gmail.com>
2020-09-09 20:52:42 +0700

    feawe

--- controllers/admin/index.js
+++ controllers/admin/index.js
@@ -334,8 +334,9 @@ router.post('/create-snapshot', async(req, res, next) => {
     var abc = path.join(global.__basedir, '../');
     // console.log(abc);
     exec('node ' + abc + 'snapshot-html/index.js ' + req.body.url + ' product-item ' + global.__basedir + '/snapshots', (error, stdout, stderr) => {
+        console.log(error, stdout, stderr);
         if (error) {
-            console.log(`error: ${error.message}`);
+            // console.log(`error: ${error.message}`);
             return res.json({ status: false, mes: 'thất bại' });
         }
         if (stderr) {
